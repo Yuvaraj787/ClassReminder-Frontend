f54e386b76bbbb5a1b659deeb4fea60a6702afa2
Dhanushit419 <dhanushit419@gmail.com>
2024-02-16 16:22:26 +0530

    merged 16/02

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 // import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//import { createStackNavigator } from '@react-navigation/stack';
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import Login from "./Pages/login";
 import { View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -13,7 +13,7 @@ import SignUp from "./Pages/Signup";
 import Notification from './Pages/Notifications';
 
 const BottomTab = createBottomTabNavigator()
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
@@ -21,6 +21,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Main" component={MainScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Notification" component={Notification} />
+        <Stack.Screen name="AddCourse" component={AddCourse} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -34,14 +35,5 @@ function MainScreen() {
       <BottomTab.Screen name="Login" component={Login} />
     </BottomTab.Navigator>
   );
-  <NavigationContainer>
-    <BottomTab.Navigator initialRouteName='DashBoard'>
-      <BottomTab.Screen name="SignUp" component={SignUp} />
-      <BottomTab.Screen name="Dashboard" component={DashBoard} />
-      <BottomTab.Screen name="Login" component={Login} />
-      <BottomTab.Screen name="Add Couse" component={AddCourse} />
-    </BottomTab.Navigator>
-  </NavigationContainer>
-  )
 
 }
